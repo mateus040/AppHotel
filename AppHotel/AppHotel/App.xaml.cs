b478f21e66be24ffd2799aa32be5e3c976478898
mateus040 <mateusgabrielmoreno264@gmail.com>
2022-08-24 15:20:16 -0300

    adicionando CultureInfo

--- AppHotel/AppHotel/App.xaml.cs
+++ AppHotel/AppHotel/App.xaml.cs
@@ -9,44 +9,48 @@ namespace AppHotel
 {
     public partial class App : Application
     {
-           public List<Model.CategoriaQuarto> tipos_quartos = new List<Model.CategoriaQuarto>()
-           {
 
-                new Model.CategoriaQuarto()
-                {
-                   Descricao = "Suíte Super Luxo",
-                   ValorDiariaAdulto = 110.0,
-                   ValorDiariaCrianca = 55.0
-                },
+         public List<Model.CategoriaQuarto> tipos_quartos = new List<Model.CategoriaQuarto>()
+         {
 
-                new Model.CategoriaQuarto()
-                {
-                    Descricao = "Suíte Luxo",
-                    ValorDiariaAdulto = 88.0,
-                    ValorDiariaCrianca = 40.0
-                },
 
-                new Model.CategoriaQuarto()
-                {
-                    Descricao = "Suíte Single",
-                    ValorDiariaAdulto = 50.0,
-                    ValorDiariaCrianca = 25.0
-                },
+             new Model.CategoriaQuarto()
+             {
+                 Descricao = "Suíte Super Luxo",
+                 ValorDiariaAdulto = 110.0,
+                 ValorDiariaCrianca = 55.0
+             },
 
-                new Model.CategoriaQuarto()
-                {
-                    Descricao = "Suíte Crise",
-                    ValorDiariaAdulto = 25.0,
-                    ValorDiariaCrianca = 12.5
-                }
-           };
+             new Model.CategoriaQuarto()
+             {
+                 Descricao = "Suíte Luxo",
+                 ValorDiariaAdulto = 88.0,
+                 ValorDiariaCrianca = 40.0
+             },
+
+             new Model.CategoriaQuarto()
+             {
+                 Descricao = "Suíte Single",
+                 ValorDiariaAdulto = 50.0,
+                 ValorDiariaCrianca = 25.0
+             },
+
+             new Model.CategoriaQuarto()
+             {
+                 Descricao = "Suíte Crise",
+                 ValorDiariaAdulto = 25.0,
+                 ValorDiariaCrianca = 12.5
+             }
+         };
 
 
         public App()
         {
             InitializeComponent();
 
-            MainPage = new MainPage();
+            Thread.CurrentThread.CurrentCulture = new CultureInfo("pt-BR");
+
+            MainPage = new NavigationPage(new View.ContratacaoHospedagem());
         }
 
         protected override void OnStart()
