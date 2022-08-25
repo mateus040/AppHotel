1f3dcdb07090f1c73b6b9d76e980febe85ce042b
mateus040 <mateusgabrielmoreno264@gmail.com>
2022-08-25 15:43:11 -0300

    criando a estrutura

--- AppHotel/AppHotel/View/ContratacaoHospedagem.xaml.cs
+++ AppHotel/AppHotel/View/ContratacaoHospedagem.xaml.cs
@@ -7,14 +7,64 @@ using System.Threading.Tasks;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
+using AppHotel.Model;
+
 namespace AppHotel.View
 {
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class ContratacaoHospedagem : ContentPage
     {
+
+        App PropriedadesApp;
+
         public ContratacaoHospedagem()
         {
             InitializeComponent();
+
+            NavigationPage.SetHasNavigationBar(this, false);
+
+            PropriedadesApp = (App)Application.Current;
+
+            pck_suites.ItemsSource = PropriedadesApp.Suites;
+
+
+
+            dtpck_checkin.MinimumDate = DateTime.Now;
+            dtpck_checkin.MaximumDate = DateTime.Now.AddMonths(6);
+
+            dtpck_ckeckout.MinimumDate = DateTime.Now;
+            dtpck_ckeckout.MaximumDate = DateTime.Now.AddMonths(6).AddDays(1);
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            try
+            {
+                Navigation.PushAsync(new HospedagemCalculada()
+                {
+                    BindingContext = new Hospedagem()
+                    {
+                        QntAdultos = Convert.ToInt32(stp_adultos.Value),
+                        QntCriancas = Convert.ToInt32(stp_criancas.Value),
+                        Quarto = (Suite)pck_suites.SelectedItem,
+                        DataCheckIn = dtpck_checkin.Date,
+                        DataCkeckOut = dtpck_ckeckout.Date
+                    }
+                });
+
+            } 
+            catch (Exception ex)
+            {
+                DisplayAlert("Ops", ex.Message, "OK");
+            }
+        }
+
+        private void dtpck_checkin_DateSelected(object sender, DateChangedEventArgs e)
+        {
+            DatePicker elemento = (DatePicker)sender;
+
+            dtpck_checkin.MinimumDate = elemento.Date.AddDays(1);
+            dtpck_ckeckout.MaximumDate = elemento.Date.AddMonths(6);
         }
     }
 }
